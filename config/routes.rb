a230b73e49f3282fa803ad71a12df773fe2045ff
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-04-09 07:23:41 +0000

    [finish3-5]

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get 'lists/new'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
